53b38de267b95f51f26cae1e31977eef537f1eb4
Maurren <maurrensantos@gmail.com>
2024-07-24 22:39:18 -0300

    Adiciona validação de entrada de dados para e-mail e telefone

--- gerenciador_contato.py
+++ gerenciador_contato.py
@@ -1,4 +1,23 @@
 import csv
+
+def adicionar_contato():
+    nome = input("Digite o nome: ")
+    telefone = input("Digite o telefone: ")
+    email = input("Digite o email: ")
+
+    with open("contatos.csv", "a", newline='') as arquivo_csv:
+        writer = csv.writer(arquivo_csv)
+        writer.writerow([nome, telefone, email])
+        print("Contato salvo com sucesso!")
+def vizualizar_contatos():
+    try:
+        with open("contatos.csv", "r") as arquivo_csv:
+            reader = csv.reader(arquivo_csv)
+            print("\nContatos salvos:")
+            for row in reader:
+                print(f"Nome: {row[0]}, Telefone: {row[1]}, E-mail: {row[2]}")
+    except FileNotFoundError:
+        print("Nenhum contato encontrado. Adicione contatos primeiro.")
 def menu_principal():
     while True:
         print ("\nSistema de Gerenciamento de Contatos")
@@ -18,20 +37,3 @@ def menu_principal():
             print("Opção inválida, tente novamente.")
 #chamando o menu principal
 menu_principal()
-def adicionar_contato():
-    nome = input("Digite o nome: ")
-    telefone = input("Digite o telefone: ")
-    email = input("Digite o email: ")
-    with open("contatos.csv","a", newline='') as arquivo_csv:
-        writer = csv.writer(arquivo_csv)
-        writer.writerow([nome, telefone, email])
-        print("Contato salvo com sucesso!")
-def vizualizar_contatos():
-    try:
-        with open("contatos.csv", "r") as arquivo_csv:
-            reader = csv.reader(arquivo_csv)
-            print("\nContatos salvos: ")
-            for row in reader:
-                print(f"Nome: {row[0]}, Telefone: {row[1]}, E-mail: {row[2]}")
-    except FileNotFoundError:
-        print("Nenhum contato encontrado. Adicione contatos primeiro.")
